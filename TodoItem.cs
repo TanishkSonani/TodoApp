8b90aea63ba12af1d5852e91f48319422637f948
Tanishk Sonani <100336884+TanishkSonani@users.noreply.github.com>
2024-10-04 19:53:19 +0530

    First console version

---
+++ TodoItem.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace TodoApp
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TodoItem(int id, string title)
+        {
+            Id = id;
+            Title = title;
+            IsCompleted = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. [ {(IsCompleted ? "X" : " ")} ] {Title}";
+        }
+    }
+}
